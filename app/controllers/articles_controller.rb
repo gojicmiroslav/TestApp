4c2516e165e99c62ced1306a8daac502d7978af6
gojicmiroslav <miroslav.gojic78@gmail.com>
2017-02-18 20:19:22 +0100

    Add REST for Article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,13 @@
 class ArticlesController < ApplicationController
+
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
 	
 	def index
 		@articles = Article.all	
 	end
 
 	def show
-		
+		@article = Article.find(params[:id])
 	end
 
 	def new
@@ -13,6 +15,41 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		
+		@article = Article.new(article_params)
+
+		if @article.save
+			redirect_to @article
+		else
+			render :new
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @article.update(article_params)
+	    redirect_to @article
+	  else
+	    render 'edit'
+	  end
+	end
+
+	def destroy
+		@article.destroy
+		redirect_to articles_path
+	end
+
+	private
+
+	def set_article
+		@article = Article.find(params[:id])
+	end
+
+	def article_params
+		accessible = [:title, :body]
+		# add new fields
+		# article_parameters << [:test]
+		params.require(:article).permit(accessible)
 	end
 end
