d1f07e606c25f83b092682a69a2c47f0739bbe08
gojicmiroslav <miroslav.gojic78@gmail.com>
2017-02-18 20:35:10 +0100

    Improve seed file

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -5,7 +5,7 @@ article1 = Article.create!(title: "First Article", body: "This error indicates t
 article2 = Article.create!(title: "Second Article", body: "A controller is simply a class that is defined to inherit from ApplicationController. It's inside this class that you'll define methods that will become the actions for this controller. These actions will perform CRUD operations on the articles within our system.")
 article2 = Article.create!(title: "Third Article", body: "A controller is simply a class that is defined to inherit from ApplicationController. It's inside this class that you'll define methods that will become the actions for this controller. These actions will perform CRUD operations on the articles within our system.")
 
-comments1 = article1.comments.build([
+comments1 = article1.comments.create!([
 	{ commenter: "Pera", body: "Some comment" },
 	{ commenter: "Foo Bar", body: "The collection.exists? method checks whether an object meeting the supplied conditions exists in the collection. It uses the same syntax and option" },	
 	{ commenter: "Jovan", body: "nds objects within the collection based on the c" },	
@@ -14,7 +14,7 @@ comments1 = article1.comments.build([
 ])
 
 
-comments2 = article2.comments.build([
+comments2 = article2.comments.create!([
 	{ commenter: "Pera", body: "Some comment" },
 	{ commenter: "Foo Bar", body: "The collection.exists? method checks whether an object meeting the supplied conditions exists in the collection. It uses the same syntax and option" },	
 	{ commenter: "Jovan", body: "nds objects within the collection based on the c" },	
