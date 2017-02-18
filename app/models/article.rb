c3e7620c7b5ce360a8bb73ac00a430fc965c35f7
gojicmiroslav <miroslav.gojic78@gmail.com>
2017-02-18 23:04:33 +0100

    Add inverse_of to Article and Comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-	has_many :comments, dependent: :destroy
+	has_many :comments, dependent: :destroy, inverse_of: :article
 
 	validates :title, presence: true, length: { minimum: 5 }
 	validates :body, presence: true
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,3 @@
 class Comment < ApplicationRecord
-	belongs_to :article
+	belongs_to :article, inverse_of: :comments
 end
